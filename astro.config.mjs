6d8f5ff6f9b6c3958b688fffec4418b459c4afbd
AF <kmhuang2500013079@gmail.com>
2026-09-23 21:07:05 +0800

    add life page

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,8 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://albertfrank-kmhuang.github.io',
   output: 'static',
+
+  redirects: {
+    '/life': '/life/music',
+  },
 });
